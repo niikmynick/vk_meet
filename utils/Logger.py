e4e3fed9e8a6decdb72a4afcd6a4645fb1814f0f
fk724s <fk724s@mail.ru>
2023-04-20 12:53:14 +0300

    took up the garbage

--- utils/Logger.py
+++ utils/Logger.py
@@ -28,16 +28,3 @@ class Logger:
     def fatal(self, message):
         self.log_file.write(f'{datetime.datetime.now()} FATAL {message}')
         self.log_file.write('\n')
-
-    def message(self, handler, profile):
-        self.log_file.write(f'{datetime.datetime.now()} MESSAGE')
-        self.log_file.write(f'New message by user: {profile[0]["first_name"]} {profile[0]["last_name"]}')
-        self.log_file.write(f'Message: {str(handler.text)}')
-
-    def user(self, user_name, user_surname, user_age, user_sex_id, user_city, user_status):
-        self.log_file.write(f'{datetime.datetime.now()} USER')
-        self.log_file.write(f'New user: {user_name} {user_surname}')
-        self.log_file.write(f'Age: {user_age}')
-        self.log_file.write(f'Sex id: {user_sex_id}')
-        self.log_file.write(f'City: {user_city}')
-        self.log_file.write(f'Status: {user_status}')
