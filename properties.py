f42e1bd87d9ba33e87d204a858b38102d2b6970e
fk724s <fk724s@mail.ru>
2023-04-20 12:48:47 +0300

    final vers

--- properties.py
+++ properties.py
@@ -1,6 +1,6 @@
 # tokens to get access
-GROUP_TOKEN = "vk1.a._AI8Wb2pyxEY6owpf7BU8AJh9yaRN6MtxuPzIROVcNBZRKqamivKTAOq-vvTv8X864ewFOfGYBriqct5X5d9IA40YyNR8uwktUHHL-FFMSC2zducVFnWDiuyWOC7jAo4snVrKu0xmJezRtCpE2D-OHKcZBA5IozQN4OylSfFA92RoQJj7Fbwm2BUEVjKyutqgmEJQuEuqoAHDSc9ZHQ6Gg"
-USER_TOKEN = "vk1.a.6nqZ9GWRvG5sPlbj_4aKC21e83v-RbwClxgtFg1fVFu1piMSHQBlkLyuEiWKCif2A2FzSdvrY36mmj1VwgsSEZIErteJfmGlac0cpMehdTj1p6j7cZHmwdeOOl4t_mfY7zRM3ZlaoxjZ5kqfEwNOf7_dReiNmTOtfYfv2RT8cFw_6WRHCfvevke5pcSet7uZgVdPgvfs--h-O0riMQuAQQ"
+GROUP_TOKEN = ""
+USER_TOKEN = ""
 
 # covert status to int and back
 status_to_int = {
